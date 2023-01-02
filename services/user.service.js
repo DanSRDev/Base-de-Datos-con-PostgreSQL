d1cc5ed56724dc85aef5b600705758bfd75a2ac5
Daniel <dansanrjsdev@hotmail.com>
2023-01-02 18:51:15 -0500

    Manejando un Pool de conexiones

--- services/user.service.js
+++ services/user.service.js
@@ -1,25 +1,40 @@
 const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
+  }
 
   async create(data) {
+    const { email, password, role } = data;
+    const query = `INSERT INTO users (email, password, role) VALUES ('${email}', '${password}', '${role}')`;
+    await this.pool.query(query);
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
+    const query = 'SELECT * FROM users';
+    const rta = await this.pool.query(query);
     return rta.rows;
   }
 
   async findOne(id) {
-    return { id };
+    const query = `SELECT * FROM users WHERE id = ${id}`;
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   async update(id, changes) {
+    const data = [];
+
+    Object.entries(changes).forEach(entry => {
+      data.push(`${entry[0]} = '${entry[1]}'`);
+    });
+    const query = `UPDATE users SET ${data.join(', ')} WHERE id = ${id}`;
+    await this.pool.query(query);
     return {
       id,
       changes,
@@ -27,6 +42,8 @@ class UserService {
   }
 
   async delete(id) {
+    const query = `DELETE FROM users WHERE id = ${id}`;
+    await this.pool.query(query);
     return { id };
   }
 }
